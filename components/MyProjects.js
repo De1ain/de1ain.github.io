286d69f174853218524d224d7fbaec4001d4c88e
Tim <borodin.tim@gmail.com>
2022-01-12 14:03:05 +0200

    MyProjects section template finished

--- components/MyProjects.js
+++ components/MyProjects.js
@@ -1,46 +1,69 @@
 import React from 'react';
+import Link from 'next/link';
 
 import styles from './MyProjects.module.css';
+import { faAngleUp } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const MyProjects = () => {
     return (
         <section className={styles['my-projects']} id="my-projects">
             <header>
-                <h3>My Projects</h3>
+                <h1>My Projects</h1>
                 <p>A selection of my projects</p>
             </header>
             <div className={styles.portfolio}>
-                <div className={styles.portfolio__item}>
-                    <h4>Project Title</h4>
-                    <p>Project description. Technologies used.</p>
-                    <span>Link to repo</span>
+                <div className={styles.portfolio__elem}>
+                    <div className={styles.portfolio__item}>
+                        <h4>Project Title</h4>
+                        <p>Project description. Technologies used.</p>
+                        <span>Link to repo</span>
+                    </div>
                 </div>
-                <div className={styles.portfolio__item}>
-                    <h4>Project Title</h4>
-                    <p>Project description. Technologies used.</p>
-                    <span>Link to repo</span>
+                <div className={styles.portfolio__elem}>
+                    <div className={styles.portfolio__item}>
+                        <h4>Project Title</h4>
+                        <p>Project description. Technologies used.</p>
+                        <span>Link to repo</span>
+                    </div>
                 </div>
-                <div className={styles.portfolio__item}>
-                    <h4>Project Title</h4>
-                    <p>Project description. Technologies used.</p>
-                    <span>Link to repo</span>
+                <div className={styles.portfolio__elem}>
+                    <div className={styles.portfolio__item}>
+                        <h4>Project Title</h4>
+                        <p>Project description. Technologies used.</p>
+                        <span>Link to repo</span>
+                    </div>
                 </div>
-                <div className={styles.portfolio__item}>
-                    <h4>Project Title</h4>
-                    <p>Project description. Technologies used.</p>
-                    <span>Link to repo</span>
+                <div className={styles.portfolio__elem}>
+                    <div className={styles.portfolio__item}>
+                        <h4>Project Title</h4>
+                        <p>Project description. Technologies used.</p>
+                        <span>Link to repo</span>
+                    </div>
                 </div>
-                <div className={styles.portfolio__item}>
-                    <h4>Project Title</h4>
-                    <p>Project description. Technologies used.</p>
-                    <span>Link to repo</span>
+                <div className={styles.portfolio__elem}>
+                    <div className={styles.portfolio__item}>
+                        <h4>Project Title</h4>
+                        <p>Project description. Technologies used.</p>
+                        <span>Link to repo</span>
+                    </div>
                 </div>
-                <div className={styles.portfolio__item}>
-                    <h4>Project Title</h4>
-                    <p>Project description. Technologies used.</p>
-                    <span>Link to repo</span>
+                <div className={styles.portfolio__elem}>
+                    <div className={styles.portfolio__item}>
+                        <h4>Project Title</h4>
+                        <p>Project description. Technologies used.</p>
+                        <span>Link to repo</span>
+                    </div>
                 </div>
             </div>
+            <footer className={styles.footer}>
+                <Link href="#top">
+                    <a className={styles['back-to-top-link']}>
+                        <FontAwesomeIcon icon={faAngleUp} />
+                        <br />Back to top
+                    </a>
+                </Link>
+            </footer>
         </section>
     );
 };
