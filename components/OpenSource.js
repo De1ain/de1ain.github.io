1a208a3093ae90b20930ad257e7be0fbeea91147
Tim <borodin.tim@gmail.com>
2022-01-12 14:58:40 +0200

    Style OpenSource section

--- components/OpenSource.js
+++ components/OpenSource.js
@@ -1,10 +1,57 @@
 import React from 'react';
+import Link from 'next/link';
+
+import styles from './OpenSource.module.css';
+import SectionFooter from './SectionFooter';
+import { faGitHub } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const OpenSource = () => {
     return (
-        <section id="open-source">
-            <h3>My Open Source Contributions</h3>
-            <p>My Open Source Contributions</p>
+        <section className={styles['open-source']} id="open-source">
+            <header>
+                <h1>My Open Source Contributions</h1>
+                <p>My Open Source Contributions</p>
+            </header>
+            <div className={styles.list}>
+                <div className={styles.list__item}>
+                    <h3>
+                        <Link href="https://github.com/mattermost/focalboard">
+                            <a className={styles.list__link} target="_blank">
+                                mattermost / focalboard
+                            </a>
+                        </Link>
+                        <FontAwesomeIcon icon={faGitHub} />
+                    </h3>
+                    <p>
+                        Focalboard is an open source, self-hosted project management tool that&apos;s an alternative to Trello, Notion, and Asana.
+                    </p>
+                    <Link href="https://github.com/mattermost/focalboard/pull/1590">
+                        <a className={styles.list__link} target="_blank">
+                            My contribution
+                        </a>
+                    </Link>
+                </div>
+                <div className={styles.list__item}>
+                    <h3>
+                        <Link href="https://github.com/Project-Reclass/toynet-react">
+                            <a className={styles.list__link} target="_blank">
+                                Project-Reclass / toynet-react
+                            </a>
+                        </Link>
+                        <FontAwesomeIcon icon={faGitHub} />
+                    </h3>
+                    <p>
+                        Project Reclass is a multi-disciplinary nonprofit organization that teaches technical skills to incarcerated veterans.
+                    </p>
+                    <Link href="https://github.com/Project-Reclass/toynet-react/pull/295">
+                        <a className={styles.list__link} target="_blank">
+                            My contributions
+                        </a>
+                    </Link>
+                </div>
+            </div>
+            <SectionFooter />
         </section>
     )
 };
