81c92527e15f2bb9deebb1fb7aab2fff0ade3159
Tim <borodin.tim@gmail.com>
2022-01-18 09:41:35 +0200

    Header now shrinks on scroll down

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 
 import styles from './Header.module.css';
 
 const Header = () => {
+    const [isShrunk, setShrunk] = useState(false);
+    let shrunkStyle;
+
+    useEffect(() => {
+        const scrollHandler = () => {
+            setShrunk((isShrunk) => {
+                if (!isShrunk && window.pageYOffset > 20) {
+                    return true;
+                }
+
+                if (isShrunk && window.pageYOffset < 4) {
+                    return false;
+                }
+
+                return isShrunk;
+            });
+        };
+
+        if (typeof window !== "undefined") {
+            window.addEventListener('scroll', scrollHandler);
+        }
+    }, []);
+
+    shrunkStyle = isShrunk ? styles['header--shrunk'] : 'xx';
+
     return (
-        <header className={styles.header} name="top">
+        <header className={`${styles.header} ${shrunkStyle}`} name="top" >
             <nav className={styles.nav}>
                 <ul className={styles.nav__list}>
                     <li className={styles.nav__item}>
-                        <Link href='#about-me'>
+                        <Link href='#top'>
                             <a className={styles.nav__link}>About Me</a>
                         </Link>
                     </li>
@@ -35,7 +60,7 @@ const Header = () => {
                     </li>
                 </ul>
             </nav>
-        </header>
+        </header >
     );
 };
 
